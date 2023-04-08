f9de9e477f909733c5e23a1f109f49f07f2e80f3
Ubuntu <ubuntu@ip-10-0-15-207.ap-northeast-1.compute.internal>
2023-04-08 13:48:24 +0000

    setup.sh を追加

---
+++ setup.sh
@@ -0,0 +1,16 @@
+sudo apt update && sudo apt upgrade
+
+### 基本 ###
+# 後半は cargo パッケージのビルドに必要
+sudo apt install \
+  vim git zsh \
+  libssl-dev pkg-config build-essential
+
+# ログインシェルの変更
+sudo chsh $USER -s $(which zsh)
+
+### dotfiles
+git clone https://github.com/Tiramister/dotfiles.git
+cd dotfiles
+./init.sh
+

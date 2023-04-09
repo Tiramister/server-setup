034e487e9b0ec0ac58469a1a162a7957fe13e61a
tiramister <nh111424@gmail.com>
2023-04-09 23:47:50 +0900

    neovim のインストール方法を appimage に変更

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 sudo apt update && sudo apt upgrade
+sudo apt install git zsh libssl-dev pkg-config build-essential
 
-### 基本 ###
-# 後半は cargo パッケージのビルドに必要
-sudo apt install \
-  neovim git zsh \
-  libssl-dev pkg-config build-essential
-
-# ログインシェルの変更
+# zsh as login shell
 sudo chsh $USER -s $(which zsh)
+
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+rm nvim.appimage
 
 # lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_arm64.tar.gz"
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm lazygit lazygit.tar.gz

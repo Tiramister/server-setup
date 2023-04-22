794e01cac289b1628dbda6666c7d04761787d760
Tiramister <nh111424@gmail.com>
2023-04-22 08:44:53 +0000

    language-pack-ja を追加

--- setup/base.sh
+++ setup/base.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade
-sudo apt install git zsh libssl-dev pkg-config build-essential
+sudo apt install git zsh libssl-dev pkg-config build-essential language-pack-ja
 
 # zsh as login shell
 sudo chsh $USER -s $(which zsh)
